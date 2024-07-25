3f9706dba9dc564d8b8a3d8565e1b5dd41c89ff8
moliwqf <2804660647@qq.com>
2024-07-25 16:04:07 +0800

    搭建定时任务demo应用

---
+++ quartz_demo/src/main/java/com/moli/schedule/QuartzDemoApplication.java
@@ -0,0 +1,17 @@
+package com.moli.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author moli
+ * @time 2024-07-25 15:56:26
+ * @description quartz 定时任务应用
+ */
+@SpringBootApplication
+public class QuartzDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzDemoApplication.class, args);
+    }
+}
